409ac0f27a6f44b1a952899080297633188ee782
outgnaY <2296176046@qq.com>
2020-09-22 11:09:10 +0800

    微调

--- data-download/src/views/Model.vue
+++ data-download/src/views/Model.vue
@@ -4,35 +4,25 @@
         <p>
             为了进一步推动研究大规模影像数据集对深度学习模型的训练和推理的影响，我们使用深度卷积神经网络训练了多标签分类模型，并使用了grad-cam方法做出类别的关注区域热力图。
         </p>
-        <p>具体的项目细节请参照我们的github，以下是项目的链接</p>
-        <div class="git-hub-link">
-            <a  href="https://github.com/WAIRlab/CRADI_x-ray_classification">https://github.com/WAIRlab/CRADI_x-ray_classification</a>
-        </div>
+        <p>具体的项目细节请参照我们的github，以下是项目的链接:&nbsp;&nbsp;<a  href="https://github.com/WAIRlab/CRADI_x-ray_classification">https://github.com/WAIRlab/CRADI_x-ray_classification</a></p>
         <br>
         <p>
-            同时，我们也将训练好的模型公布如下：
+            同时，我们也将训练好的模型公布如下：&nbsp;&nbsp;<a href="http://202.120.39.3:25488/api/v1/file/download_model?path=/Data-CRADI&name=model.zip"> download_link</a>
         </p>
-        <div class="model-link">
-            <a> downloadlink</a>
-        </div>
+
         </div>
         <div class="english" v-else>
         <p>
              In order to further promote the research on the influence of large-scale image data sets on the training and reasoning of deep learning model, we train the multi-label classification model based on the CNN and make the thermal map of the concerned area of the category through the grad cam method.
         </p>
         <p>
-             You can find the details about this project in our github, here is the link:
+             You can find the details about this project in our github, here is the link:&nbsp;&nbsp;<a  href="https://github.com/WAIRlab/CRADI_x-ray_classification">https://github.com/WAIRlab/CRADI_x-ray_classification</a>
         </p>
-        <div class="git-hub-link">
-            <a  href="https://github.com/WAIRlab/CRADI_x-ray_classification">https://github.com/WAIRlab/CRADI_x-ray_classification</a>
-        </div>
+
         <br>
         <p>
-            At the same time, we pulish the trained model as follows: 
+            At the same time, we pulish the trained model as follows:&nbsp;&nbsp;<a href="http://202.120.39.3:25488/api/v1/file/download_model?path=/Data-CRADI&name=model.zip"> download_link</a>
         </p>
-        <div class="model-link">
-            <a> downloadlink</a>
-        </div>
         </div>
         <br>
         <br>
@@ -71,6 +61,9 @@ export default {
       font-size: 15px;
       line-height: 1.8;
       margin-bottom: 10px;
+    }
+    a {
+      color: #2d8cf0;
     }
     .git-hub-link {
         text-align: center;
